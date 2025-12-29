42b0babe7855992c99f00fb7bee3e5f262698e22
Peixin <2892078398@qq.com>
2025-12-30 00:51:24 +0800

    修改CTRL.v
    Updated stall request handling logic in CTRL module.

--- CTRL.v
+++ CTRL.v
@@ -1,15 +1,22 @@
 `include "lib/defines.vh"
 module CTRL(
     input wire rst,
-    input wire stallreq,
+    input wire stallreq_for_load,
+    input wire stallreq_from_ex, 
 
     output reg [`StallBus-1:0] stall
-);  
+);
+
     always @ (*) begin
         if (rst) begin
             stall = `StallBus'b0;
         end
-        else if (stallreq) begin
+        else if (stallreq_from_ex) begin
+            // EX请求暂停（除法）：暂停 PC, IF, ID, EX
+            stall = 6'b001111; 
+        end
+        else if (stallreq_for_load) begin
+            // Load请求暂停：暂停 PC, IF, ID
             stall = 6'b000111;
         end
         else begin
